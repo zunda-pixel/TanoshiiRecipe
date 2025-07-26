e9405cdbfb5f36dbadb15c22032eee4bdf7b3e70
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-07-26 16:27:21 +0900

    RecordViewを追加

---
+++ TanoshiiRecipeKit/Sources/TanoshiiRecipeUI/RecordView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct RecordView: View {
+  @State var router = NavigationRouter()
+  
+  var body: some View {
+    NavigationStack(path: $router.path) {
+      List {
+        Button {
+          router.path.append(.cookingRecordAlbum)
+        } label: {
+          Text("Cooking Record Album")
+        }
+      }
+      .navigationDestination(for: NavigationRouter.Item.self) { item in
+        switch item {
+        case .cookingRecordAlbum:
+          CookingRecordAlbumView()
+        }
+      }
+    }
+    .environment(router)
+  }
+}
