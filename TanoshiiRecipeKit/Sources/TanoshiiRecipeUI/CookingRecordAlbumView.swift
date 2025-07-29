888eb092de56ddaa966ef8c04d16603b3d319f65
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-07-29 10:12:26 +0900

    CookingRecordAlbumViewからCookingRecordDetailViewへsheetで画面遷移するように

--- TanoshiiRecipeKit/Sources/TanoshiiRecipeUI/CookingRecordAlbumView.swift
+++ TanoshiiRecipeKit/Sources/TanoshiiRecipeUI/CookingRecordAlbumView.swift
@@ -10,10 +10,13 @@ struct CookingRecordAlbumView: View {
     @ObservationIgnored var pagination: Pagination?
     let client = Client()
     var isLoading = false
+    
+    var selectedCookingRecord: CookingRecord?
   }
-
-  var model = Model()
-
+  
+  @State var model = Model()
+  @Namespace var namespace
+  
   func fetchData() async {
     guard model.isLoading == false else { return }
     defer { model.isLoading = false }
@@ -42,21 +45,38 @@ struct CookingRecordAlbumView: View {
       KFImage(cookingRecord.imageUrl)
         .resizable()
         .aspectRatio(1, contentMode: .fit)
+        .clipShape(.rect(cornerRadius: 8))
+        .overlay(alignment: .topLeading) {
+          Text(cookingRecord.recipeType.label)
+            .padding(5)
+            .background(.regularMaterial, in: .capsule)
+            .padding(5)
+        }
     }
   }
 
   var body: some View {
     ScrollView(.vertical) {
       LazyVGrid(columns: [.init(), .init()]) {
-        ForEach(model.cookingRecords, id: \.self) { cookingRecord in
+        ForEach(model.cookingRecords) { cookingRecord in
           CellView(cookingRecord: cookingRecord)
+            .matchedTransitionSource(id: cookingRecord, in: namespace)
             .task {
               guard cookingRecord == model.cookingRecords.last else { return }
               await fetchData()
             }
+            .contentShape(.rect)
+            .onTapGesture {
+              model.selectedCookingRecord = cookingRecord
+            }
         }
       }
     }
+    .sheet(item: $model.selectedCookingRecord) { cookingRecord in
+      CookingRecordDetailView(cookingRecord: cookingRecord)
+        .navigationTransition(.zoom(sourceID: cookingRecord, in: namespace))
+    }
+    .padding(.horizontal, 8)
     .navigationTitle(Text("Cooking Record Album"))
     .refreshable {
       await fetchData()
@@ -73,3 +93,16 @@ struct CookingRecordAlbumView: View {
   }
 }
 
+extension CookingRecord: Identifiable {
+  public var id: Self { self }
+}
+
+extension RecepeType {
+  var label: LocalizedStringKey {
+    switch self {
+    case .mainDish: "Main Dith"
+    case .sideDish: "Side Dish"
+    case .soup: "Soup"
+    }
+  }
+}
